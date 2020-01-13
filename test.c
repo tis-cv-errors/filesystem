b138a7097c8c4a7244bbeddb931cdec0f06d2e2a
Guillaume Millot <guillaume.millot@gmail.com>
2020-01-13 12:07:37 +0100

    Update test.c

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 int main (void)
 {
   FILE * f = fopen ("input.txt", "r");
-  if (f) {
+  //if (f) {
     int c = fgetc (f);
     if (c != EOF)
       printf ("First char = '%c'\n", (char)c);
     fclose (f);
-  }
+  //}
 }
