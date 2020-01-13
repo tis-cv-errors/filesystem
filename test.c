2f91ce6fa20903d858b76011d9f7db5d9d9b83fc
Guillaume Millot <guillaume.millot@gmail.com>
2020-01-13 12:03:14 +0100

    Create test.c

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+  FILE * f = fopen ("input.txt", "r");
+  if (f) {
+    int c = fgetc (f);
+    if (c != EOF)
+      printf ("First char = '%c'\n", (char)c);
+    fclose (f);
+  }
+}
