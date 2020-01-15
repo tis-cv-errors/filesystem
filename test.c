86ad11d8b0fddca00ce4c5f7fa5e90925a05253c
Guillaume Millot <guillaume.millot@gmail.com>
2020-01-15 18:37:37 +0100

    Update test.c

--- test.c
+++ test.c
@@ -2,11 +2,19 @@
 
 int main (void)
 {
-  FILE * f = fopen ("input2.txt", "w");
+  FILE * f = fopen ("input1.txt", "r");
   if (f) {
     int c = fgetc (f);
     if (c != EOF)
       printf ("First char = '%c'\n", (char)c);
+    fclose (f);
+  }
+  
+  char str[] = "This is tutorialspoint.com";$
+    
+  FILE * f2 = fopen ("input2.txt", "w");
+  if (f2) {
+    fwrite(str , 1 , sizeof(str) , f2);
     fclose (f);
   }
 }
